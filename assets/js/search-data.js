2318b10bfbb2235749c31241a4a045ff0c97b9cc
shuo-zhou <sz144@outlook.com>
2025-03-11 23:56:09 +0000

    Deploying to gh-pages from @ shuo-zhou/shuo-zhou.github.io@f492c4f8785a39bb4a13cc058dafa4a19d80e9b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,14 +474,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%75%6F.%7A%68%6F%75@%73%68%65%66%66%69%65%6C%64.%61%63.%75%6B", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/shuo-zhou", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/shuo-zhou-02754763", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-8069-2814", "_blank");
         },
       },{
         id: 'social-rss',
@@ -495,7 +509,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=b90lZb4AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
